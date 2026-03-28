348805e0cb9859519063e490e77a3e2eb76b3f8f
aabdullahsayed <abdullahalsayed087@gmail.com>
2026-03-28 23:11:51 +0600

    Add Service Directory

---
+++ Services/IProductService.cs
@@ -0,0 +1,9 @@
+﻿namespace Minimal_API.Services;
+using Models;
+public interface IProductServices
+{
+    List<Product> getAll();
+    Product getbyID(int id);
+    Product Add(Product product);
+    
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿namespace Minimal_API.Services;
+using Models;
+
+public class ProductService:IProductService
+{
+
+    private readonly List<Product> _products = new List<Product>();
+
+    public List<Product> getAll()
+    {
+        return _products;
+    }
+
+    public Product getbyID(int id)
+    {
+
+        return _products.FirstOrDefault(p => p.Id == id);
+
+    }
+
+    public Product Add(Product product)
+    {
+        product.Id = _products.Count + 1;
+        
+        _products.Add(product);
+
+        return product;
+    }
+}
